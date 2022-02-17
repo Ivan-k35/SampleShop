1125521fb953c1c7aaa329db9f6cb177b29fbf12
Ivan-k35 <ivan.koshkin35@gmail.com>
2022-02-17 12:09:56 +0300

    Изменил файлы admin.py, specifications.py для корректного занесения информации в панели администратора о наличии или отсутствии карты памяти у смартфонов.

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -5,34 +5,23 @@ from django.utils.safestring import mark_safe
 from .models import *
 
 
-class NotebookAdminForm(ModelForm):
-    """Проверка загружаемого изображения на соответствие минимальному и максимальному разрешению"""
-    MIN_RESOLUTION = (400, 400)
-    MAX_RESOLUTION = (1200, 1200)
-    MAX_IMAGE_SIZE = 3145728
-
+class SmartphoneAdminForm(ModelForm):
+    """Изменяет панель администратора в зависимости от наличия или отсутствия SD карты у телефона"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text = mark_safe('<span style="color:red; font-size:14px;">Загружайте изображения с '
-                                                   'минимальным разрешением {}x{}</span>'.format(*self.MIN_RESOLUTION))
+        instance = kwargs.get('instance')
+        if not instance.sd:
+            self.fields['sd_volume_max'].widget.attrs.update({
+                'readonly': True, 'style': 'background: lightgray'
+            })
 
-    def clean_image(self):
-        image = self.cleaned_data['image']
-        img = Image.open(image)
-        min_height, min_width = self.MIN_RESOLUTION
-        max_height, max_width = self.MAX_RESOLUTION
-        if image.size > self.MAX_IMAGE_SIZE:
-            raise ValidationError('Размер изображения не должен превышать 3МБ!')
-        if img.height < min_height or img.width < min_width:
-            raise ValidationError('Разрешение изображения меньше минимального')
-        if img.height > max_height or img.width > max_width:
-            raise ValidationError('Разрешение изображения больше максимального')
-        return image
+    def clean(self):
+        if not self.cleaned_data['sd']:
+            self.cleaned_data['sd_volume_max'] = None
+        return self.cleaned_data
 
 
 class NotebookAdmin(admin.ModelAdmin):
-
-    form = NotebookAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
@@ -43,6 +32,7 @@ class NotebookAdmin(admin.ModelAdmin):
 class SmartphoneAdmin(admin.ModelAdmin):
 
     change_form_template = 'mainapp/admin.html'
+    form = SmartphoneAdminForm
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
--- mainapp/templatetags/specifications.py
+++ mainapp/templatetags/specifications.py
@@ -1,6 +1,8 @@
 from django import template
 from django.utils.safestring import mark_safe
 
+from mainapp.models import Smartphone
+
 register = template.Library()
 
 
@@ -54,4 +56,9 @@ def get_product_spec(product, model_name):
 @register.filter
 def product_spec(product):
     model_name = product.__class__._meta.model_name
+    if isinstance(product, Smartphone):
+        if not product.sd:
+            PRODUCT_SPEC['smartphone'].pop('Максимальный объем карты памяти')
+        else:
+            PRODUCT_SPEC['smartphone']['Максимальный объем карты памяти'] = 'sd_volume_max'
     return mark_safe(TABLE_HEAD + get_product_spec(product, model_name) + TABLE_TAIL)
